a4a7242227f2a308c5744390fc9313115ba2ed60
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-16 17:48:20 -0400

    adds useQuery(Get_me) in savedBooks.js and refactored from api getMe

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -39,7 +39,7 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
-export const REMOVE_BOOk = gql`
+export const REMOVE_BOOK = gql`
   mutation removeBook(bookId: ID!){
     removeBook(bookId: $bookID){
         _id
