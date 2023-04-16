dfd1787d1ba003ff99832b33c448af47db003b67
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-16 17:19:31 -0400

    adds mutation.js and logic in client utils

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
     mutation login(username: String!, email: String!, password: String!){
-        login(username: String!, email: String!, password: String!){
+        login(username: $username, email: $email, password: $password){
             token
             user {
               _id
@@ -11,14 +11,44 @@ export const LOGIN = gql`
         }
     }
 `;
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+export const ADD_USER = gql`
+  mutation addUser(username: String!, email: String!, password: String!){
+    addUser(username: $username, email: $email, password: $password){
       token
-      profile {
+      user {
         _id
-        name
+        username
       }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(bookPieces: BookInput!) {
+    saveBook(bookPieces: $bookpieces) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+export const REMOVE_BOOk = gql`
+  mutation removeBook(bookId: ID!){
+    removeBook(bookId: $bookID){
+        _id
+        username
+        savedBooks{
+            bookId
+            author
+            title
+        }
     }
   }
 `;
