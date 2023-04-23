1748f7cf607cafc12d3527a67c3de4c8cbf94566
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-23 15:42:56 -0400

    fixed me query in resolvers and adds useEffect back in SignupForm

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
@@ -18,6 +18,14 @@ const SignupForm = () => {
   const [showAlert, setShowAlert] = useState(false);
 
   const [addUser, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -30,7 +30,7 @@ const typeDefs = gql`
   }
 
   type Query {
-    me: User
+    me(username: String!): User
   }
 
   type Mutation {
